7509c72f8f313cb762103957fb36e4f66430a5b4
acidburn7 <varaksinaalla@mail.ru>
2023-12-18 12:43:25 +0300

    create column

--- mysite/boards/urls.py
+++ mysite/boards/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, create_column, create_task, delete
+from .views import index, create_column, create_task, delete, delete_board, delete_column
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:board_id>/create-column/', create_column, name='create-column'),
     path('<int:column_id>/create-task/', create_task, name='create-task'),
-    path('<int:task_id>/delete/', delete, name='delete')
+    path('<int:task_id>/delete/', delete, name='delete'),
+    path('<int:board_id>/delete-board/', delete_board, name='delete-board'),
+    path('<int:column_id>/delete-column/', delete_column, name='delete-column')
 ]
--- mysite/boards/views.py
+++ mysite/boards/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from .models import Board, Column, Task
 
+
 def index(request):
     board = Board.objects.get(name="Проект трелло")
     columns = Column.objects.filter(board=board)
@@ -15,14 +16,30 @@ def index(request):
     }
     return render(request, "boards/index.html", context)
 
+
 def create_task(request, column_id):
     Task.objects.create(title=request.POST["title"], description="", column_id=column_id)
     return redirect('index')
 
-def create_column(request, column_id):
-    Task.objects.create(title=request.POST["title"], description="", column_id=column_id)
+
+def create_column(request, board_id):
+    Column.objects.create(name=request.POST["name"], board_id=board_id)
     return redirect('index')
+
 
 def delete(request, task_id):
     Task.objects.get(id=task_id).delete()
+    return redirect('index')
+
+
+def delete_column(request, column_id):
+    Column.objects.get(id=column_id).delete()
+    return redirect('index')
+
+def delete_board(request, board_id):
+    return redirect('index')
+
+def create_board(request):
+    Task.objects.create(title=request.POST["title"])
+
     return redirect('index')
